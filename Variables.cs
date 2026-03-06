9bc668365724d6feb14dae42db3f9ea5f20ea94b
Ritaja <tonnitarafder2004@gmail.com>
2026-03-06 15:13:40 +0530

    Add project files.

---
+++ Variables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasics
+    S
+{ 
+    internal class Variables
+    {
+        static void Main(string[] args)
+        {
+        Console.Write("Enter the number:");
+        int num1 = Convert.ToInt32(Console.ReadLine());// direct conversion from string to num
+        Console.Wrie("Enter another number:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(num1 + num2);
+        }
+    }
+}
